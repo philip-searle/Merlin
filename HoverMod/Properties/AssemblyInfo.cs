242def855c73bc6643fcf7521609546d308db6ea
Philip Searle <philip@philip-searle.me.uk>
2012-11-23 22:07:06 +0000

    Rename DebugProject to HoverMod
    It's not just a scratchpad anymore; it's going to be a fully-fledged
    Hover modding tool!

--- HoverMod/Properties/AssemblyInfo.cs
+++ HoverMod/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DebugProject")]
+[assembly: AssemblyTitle("HoverMod")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DebugProject")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyProduct("HoverMod")]
+[assembly: AssemblyCopyright("Copyright ©  2012 Philip Searle")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
