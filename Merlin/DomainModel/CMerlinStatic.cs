70192ed27040db28eecb37172e2fcb07a4009ba8
Philip Searle <philip@philip-searle.me.uk>
2012-12-11 00:04:51 +0000

    Getting a better handle on how textures are applied to linedefs.

--- Merlin/DomainModel/CMerlinStatic.cs
+++ Merlin/DomainModel/CMerlinStatic.cs
@@ -9,19 +9,19 @@ namespace Merlin.DomainModel
     [MfcSerialisable("CMerlinStatic")]
     public class CMerlinStatic : CMerlinLine
     {
-        public string unknown08; // Ceiling texture 1?
-        public string unknown09; // Ceiling texture 2?
-        public string unknown10; // Wall texture, left side?
-        public string unknown11; // Wall texture, right side?
-        public string unknown12; // Floor texture 1?
-        public string unknown13; // Floor texture 2?
+        public string unknown08; // left side: draws from top of linedef to top of screen ('inside polygon looking out', i.e. ceiling)
+        public string unknown09; // right side: draws from bottom of linedef to bottom of screen ('outside polygon looking in', i.e. floor)
+        public string unknown10; // Wall texture, left side - confirmed
+        public string unknown11; // Wall texture, right side - confirmed
+        public string unknown12; // left side: draws from bottom of linedef to bottom of screen ('inside polygon looking out', i.e. floor)
+        public string unknown13; // right side: draws from top of linedef to top of screen ('outside polygon looking in', i.e. ceiling)
         public ushort BottomZ;
         public ushort TopZ;
         public ushort unknown16;
         public ushort unknown17;
         public byte unknown18;  // Flag? Only seems to be set on decals.
         public byte unknown19;  // Flag? Only seems to be set on decals.
-        public byte unknown20;  // Maybe collision flag? Not set on pad boundaries.
+        public byte unknown20;  // If set, enable collision. If not set, no collision.
         public ushort unknown21;
         public byte unknown22;
         public ushort unknown23;
